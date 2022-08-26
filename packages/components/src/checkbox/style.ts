9f99c2e029ae968dac9aac7021884eddbcad0cbb
guolinjie <guolinjie@prevailcloud.com>
2022-08-26 14:44:26 +0800

    删除无用组件

--- packages/components/src/checkbox/style.ts
+++
@@ -1,5 +0,0 @@
-import 'element-plus/theme-chalk/src/checkbox.scss'
-import 'element-plus/theme-chalk/src/checkbox-group.scss'
-import 'element-plus/theme-chalk/src/checkbox-button.scss'
-// 依赖
-import '../preview-text/style'
